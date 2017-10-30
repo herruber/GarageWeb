3cb1cadb2b5390b345578e8f0dc702c69e25fdb4
Robert <robertnyquist.rn@gmail.com>
2017-10-30 10:25:13 +0100

    Project creation

---
+++ GarageWeb/DataAccess/GarageContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace GarageWeb.DataAccess
+{
+    public class GarageContext : DbContext
+    {
+
+        public DbSet<Models.Vehicle> Vehicles { get; set; }
+
+
+        public GarageContext() : base()
+        {
+
+        }
+    }
+}
---
+++ GarageWeb/Models/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace GarageWeb.Models
+{
+    public class Vehicle
+    {
+
+
+        [Key]
+        public string Regnr { get; set; }
+        public string Persnr {get; set; }
+        public DateTime ParkDate {get; set;}
+        
+        public enum vType
+	        {
+	         Car,
+            Motorcycle,
+            Bus,
+            Truck
+	        };
+
+        public vType VehicleType { get; set; }
+
+    }
+}
