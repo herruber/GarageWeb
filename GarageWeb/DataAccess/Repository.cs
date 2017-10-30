688567ff7fe7a3b41c9b97bfa8f1127985727731
Robert <robertnyquist.rn@gmail.com>
2017-10-30 10:43:37 +0100

    Repository

---
+++ GarageWeb/DataAccess/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GarageWeb.DataAccess
+{
+    public class Repository
+    {
+
+        GarageContext gC = new GarageContext();
+
+        public IEnumerable<Models.Vehicle> GetStock()
+        {
+            var tempStock = gC.Vehicles;
+            return tempStock;
+        }
+    }
+}
